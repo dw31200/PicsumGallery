1bdfc4d5a8ff4b3bbd5e691e0b4e094a0fe92006
dw31200 <sdw31200@naver.com>
2023-04-24 11:29:55 +0900

    #4 Retrofit 코루틴 콜로 변경

--- app/src/main/java/com/example/picsumgallery/ui/GalleryFragment.kt
+++ app/src/main/java/com/example/picsumgallery/ui/GalleryFragment.kt
@@ -1,21 +1,19 @@
 package com.example.picsumgallery.ui
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
+import androidx.fragment.app.replace
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.picsumgallery.R
-import com.example.picsumgallery.data.Picsum
 import com.example.picsumgallery.databinding.FragmentGalleryBinding
 import com.example.picsumgallery.network.PicsumApi
 import com.example.picsumgallery.ui.GalleryDetailFragment.Companion.args
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import kotlinx.coroutines.launch
 
 class GalleryFragment : Fragment() {
     private var _binding: FragmentGalleryBinding? = null
@@ -39,27 +37,17 @@ class GalleryFragment : Fragment() {
         binding.galleryList.adapter = GalleryAdapter { galleryId -> adapterOnClick(galleryId) }
         binding.galleryList.layoutManager = GridLayoutManager(context, 2)
 
-        PicsumApi.retrofitService.fetchContents().enqueue(object : Callback<List<Picsum>> {
-            override fun onResponse(call: Call<List<Picsum>>, response: Response<List<Picsum>>) {
-                Log.d("GalleryFragment", "Response received ${response.body()}")
-                response.body()?.let {
-                    (binding.galleryList.adapter as GalleryAdapter).fetchData(it)
-                }
-            }
-
-            override fun onFailure(call: Call<List<Picsum>>, t: Throwable) {
-                Log.e("GalleryFragment", "Failed to fetch image", t)
-            }
-        })
+        lifecycleScope.launch {
+            val list = PicsumApi.retrofitService.fetchContents()
+            (binding.galleryList.adapter as GalleryAdapter).fetchData(list)
+        }
     }
 
     private fun adapterOnClick(galleryId: Int) {
         parentFragmentManager.commit {
-            replace(
+            replace<GalleryDetailFragment>(
                 R.id.fragment_container,
-                GalleryDetailFragment().apply {
-                    arguments = args(galleryId)
-                },
+                args = args(galleryId),
             )
             addToBackStack(null)
         }
