7d8b6a4738b660d27d59e3aad5932d2e8fdc3374
dw31200 <sdw31200@naver.com>
2023-05-16 21:21:27 +0900

    #8 okhttp 설정

--- app/src/main/java/com/example/picsumgallery/network/PicsumApi.kt
+++ app/src/main/java/com/example/picsumgallery/network/PicsumApi.kt
@@ -1,16 +1,32 @@
 package com.example.picsumgallery.network
 
+import com.example.picsumgallery.BuildConfig
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
 
 object PicsumApi {
     private const val BASE_URL = "https://picsum.photos/"
+    private val interceptor = HttpLoggingInterceptor().apply {
+        level = if (BuildConfig.DEBUG) {
+            HttpLoggingInterceptor.Level.BODY
+        } else {
+            HttpLoggingInterceptor.Level.NONE
+        }
+    }
+    private val okHttpClient = OkHttpClient.Builder()
+        .connectTimeout(20L, TimeUnit.SECONDS)
+        .addInterceptor(interceptor)
+        .build()
     private val moshi = Moshi.Builder()
         .add(KotlinJsonAdapterFactory())
         .build()
     private val retrofit = Retrofit.Builder()
+        .client(okHttpClient)
         .addConverterFactory(MoshiConverterFactory.create(moshi))
         .baseUrl(BASE_URL)
         .build()
