8125ea0aaaf0fa17ff539810886ca8005f59306e
dw31200 <sdw31200@naver.com>
2023-08-09 20:02:41 +0900

    #22 Gallery Like -> Like만 가져오기 (UseCaes 별도 존재)

---
+++ domain/src/main/java/com/example/picsumgallery/domain/usecase/GetLikeItemListUseCase.kt
@@ -0,0 +1,32 @@
+package com.example.picsumgallery.domain.usecase
+
+import com.example.picsumgallery.data.model.PicsumLike
+import com.example.picsumgallery.data.repository.PicsumLikeRepository
+import com.example.picsumgallery.data.repository.PicsumRepository
+import com.example.picsumgallery.domain.model.PicsumEntity
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class GetLikeItemListUseCase @Inject constructor(
+    private val picsumRepository: PicsumRepository,
+    private val picsumLikeRepository: PicsumLikeRepository,
+) {
+    operator fun invoke(): Flow<List<PicsumEntity>> {
+        val itemList = picsumRepository.getAll().map {
+            it.map {
+                PicsumEntity(it)
+            }
+        }
+        val likeItemList = flow { emit(picsumLikeRepository.getAll()) }
+
+        return combine(itemList, likeItemList) { _itemList, _likeItemList ->
+            _itemList.filter { _likeItemList.contains(PicsumLike(it.id)) }
+                .map {
+                    it.copy(isLiked = true)
+                }
+        }
+    }
+}
--- ui/src/main/java/com/example/picsumgallery/ui/like/LikeFragment.kt
+++ ui/src/main/java/com/example/picsumgallery/ui/like/LikeFragment.kt
@@ -5,12 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.example.picsumgallery.ui.databinding.FragmentLikeBinding
+import com.example.picsumgallery.ui.list.adapter.GalleryAdapter
+import com.example.picsumgallery.ui.list.adapter.GalleryItemDecoration
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class LikeFragment : Fragment() {
     private var _binding: FragmentLikeBinding? = null
     private val binding
         get() = _binding!!
+    private val viewModel: LikeViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,5 +26,21 @@ class LikeFragment : Fragment() {
         super.onCreateView(inflater, container, savedInstanceState)
         _binding = FragmentLikeBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.vm = viewModel
+        binding.lifecycleOwner = this@LikeFragment
+        binding.galleryLikeitemList.adapter = GalleryAdapter()
+        binding.galleryLikeitemList.addItemDecoration(GalleryItemDecoration())
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
