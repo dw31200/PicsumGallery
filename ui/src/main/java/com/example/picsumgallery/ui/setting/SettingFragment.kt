8ae545fb903af6445a4a3997dee02e4afd28f2da
dw31200 <sdw31200@naver.com>
2023-08-21 18:14:03 +0900

    #24 누락된 lifecyclerOwner 추가

--- ui/src/main/java/com/example/picsumgallery/ui/setting/SettingFragment.kt
+++ ui/src/main/java/com/example/picsumgallery/ui/setting/SettingFragment.kt
@@ -31,6 +31,7 @@ class SettingFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.vm = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
         binding.systemButton.setOnClickListener {
             lifecycleScope.launch { viewModel.setTheme(0) }
         }
