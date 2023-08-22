3650cf0f57e862680e33e2ab93c515b24da4a594
dw31200 <sdw31200@naver.com>
2023-08-22 20:57:54 +0900

    #26 List - Remote 완료, gallerylist 에 like 데이터 적용, detailviewmodel savedStateHandle 수정

--- data/src/main/java/com/example/picsumgallery/data/paging/GalleryListPagingSourceFactoryImpl.kt
+++ data/src/main/java/com/example/picsumgallery/data/paging/GalleryListPagingSourceFactoryImpl.kt
@@ -4,15 +4,19 @@ import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import com.example.picsumgallery.data.local.PicsumDao
 import com.example.picsumgallery.data.model.Picsum
+import com.example.picsumgallery.data.remote.Limit
+import com.example.picsumgallery.data.remote.PicsumApiService
 import javax.inject.Inject
 
 class GalleryListPagingSourceFactoryImpl @Inject constructor(
     private val picsumDao: PicsumDao,
+    private val picsumApiService: PicsumApiService,
+    @Limit private val limit: Int,
 ) : GalleryListPagingSourceFactory {
     override fun getPagingData(): Pager<Int, Picsum> {
         return Pager(
-            PagingConfig(pageSize = 30),
-            pagingSourceFactory = { GalleryListPagingSource(picsumDao) },
+            PagingConfig(pageSize = limit),
+            pagingSourceFactory = { GalleryListPagingSource(picsumDao, picsumApiService, limit) },
         )
     }
 }
--- domain/src/main/java/com/example/picsumgallery/domain/usecase/GetItemListUseCase.kt
+++ domain/src/main/java/com/example/picsumgallery/domain/usecase/GetItemListUseCase.kt
@@ -2,20 +2,29 @@ package com.example.picsumgallery.domain.usecase
 
 import androidx.paging.PagingData
 import androidx.paging.map
+import com.example.picsumgallery.data.model.PicsumLike
 import com.example.picsumgallery.data.paging.GalleryListPagingSourceFactory
+import com.example.picsumgallery.data.repository.PicsumLikeRepository
 import com.example.picsumgallery.domain.model.PicsumEntity
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class GetItemListUseCase @Inject constructor(
     private val galleryListPagingSourceFactory: GalleryListPagingSourceFactory,
+    private val picsumLikeRepository: PicsumLikeRepository,
 ) {
     operator fun invoke(): Flow<PagingData<PicsumEntity>> {
-        return galleryListPagingSourceFactory.getPagingData().flow.map {
+        val likeItemList = picsumLikeRepository.getAll()
+        val itemList = galleryListPagingSourceFactory.getPagingData().flow.map {
             it.map { picsum ->
                 PicsumEntity(picsum)
             }
+        }
+
+        return combine(itemList, likeItemList) { _itemList, _likeItemList ->
+            _itemList.map { it.copy(isLiked = _likeItemList.contains(PicsumLike(it.id))) }
         }
     }
 }
