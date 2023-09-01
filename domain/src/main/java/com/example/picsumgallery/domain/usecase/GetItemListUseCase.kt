5a35d313c74a7f1bca26f5c406a3c2c239602f18
dw31200 <sdw31200@naver.com>
2023-09-01 17:30:27 +0900

    #26 paging cachedIn 오류 수정

--- domain/src/main/java/com/example/picsumgallery/domain/usecase/GetItemListUseCase.kt
+++ domain/src/main/java/com/example/picsumgallery/domain/usecase/GetItemListUseCase.kt
@@ -1,11 +1,14 @@
 package com.example.picsumgallery.domain.usecase
 
 import androidx.paging.PagingData
+import androidx.paging.cachedIn
 import androidx.paging.map
 import com.example.picsumgallery.data.model.PicsumLike
 import com.example.picsumgallery.data.paging.GalleryListPagingSourceFactory
 import com.example.picsumgallery.data.repository.PicsumLikeRepository
 import com.example.picsumgallery.domain.model.PicsumEntity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.map
@@ -21,7 +24,7 @@ class GetItemListUseCase @Inject constructor(
             it.map { picsum ->
                 PicsumEntity(picsum)
             }
-        }
+        }.cachedIn(CoroutineScope(Dispatchers.IO))
 
         return combine(itemList, likeItemList) { _itemList, _likeItemList ->
             _itemList.map { it.copy(isLiked = _likeItemList.contains(PicsumLike(it.id))) }
